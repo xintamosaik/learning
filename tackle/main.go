d7e020602e89d83e59a06600235b3b7f55e12eb4
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-13 23:13:39 +0200

    it works

--- tackle/main.go
+++ tackle/main.go
@@ -1,48 +1,56 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "strings"
-  "unicode"
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
 )
 
+func readTasks(filename string) (scanner *bufio.Scanner, err error) {
 
-func main() {
-  const FILENAME = "todo.md"
-  fmt.Println("started..")
-  file, err := os.Open(FILENAME)
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-    defer file.Close()
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, errors.New("file not found")
+	}
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := scanner.Text()
-        trimmed := strings.TrimSpace(line)
-      
-        if len(trimmed) > 0 {
-     
-    
-    
-          if unicode.IsDigit(rune(line[0])) {
-            fmt.Println("[epic] "+ trimmed)
-          }
-          if rune(line[0]) == 32 {
-            fmt.Println("[task] "+ trimmed)
-          }
+	scanner = bufio.NewScanner(file)
 
-        } else {
-          fmt.Println("[empty]")
-        }
-    }
+	if err := scanner.Err(); err != nil {
+		return nil, errors.New("something went wrong with the file reading")
+	}
 
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error:", err)
-    }
-
+	return scanner, nil
 }
 
+func main() {
+	const FILENAME = "todo.md"
+  
+	fmt.Println("started..")
+	scanner, err := readTasks(FILENAME)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		trimmed := strings.TrimSpace(line)
+
+		if len(trimmed) > 0 {
+			if unicode.IsDigit(rune(line[0])) {
+				fmt.Println("[epic] " + trimmed)
+			}
+			if rune(line[0]) == 32 {
+				fmt.Println("[task] " + trimmed)
+			}
+
+		} else {
+			fmt.Println("[empty]")
+		}
+	}
+}
