d988187089737911cb4a9a5f2412df329442ca06
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-13 23:52:09 +0200

    experiment on structs and slices

--- tackle/main.go
+++ tackle/main.go
@@ -2,39 +2,43 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 	"unicode"
 )
+type Task struct {
+	title string
+	description string
+}
 
-func readTasks(filename string) (scanner *bufio.Scanner, err error) {
 
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return nil, errors.New("file not found")
-	}
+type Epic struct {
+	title string
+	tasks []Task
+}
 
-	scanner = bufio.NewScanner(file)
-
-	if err := scanner.Err(); err != nil {
-		return nil, errors.New("something went wrong with the file reading")
-	}
-
-	return scanner, nil
+type Project struct {
+	title string
+	epics []Epic
 }
 
 func main() {
+  fmt.Println("started..")
 	const FILENAME = "todo.md"
-  
-	fmt.Println("started..")
-	scanner, err := readTasks(FILENAME)
+
+	file, err := os.Open(FILENAME)
 	if err != nil {
-		log.Fatal(err)
-		return
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	if err := scanner.Err(); err != nil {
+    log.Fatal(err)
+		os.Exit(1)
 	}
 
 	for scanner.Scan() {
@@ -53,4 +57,24 @@ func main() {
 			fmt.Println("[empty]")
 		}
 	}
+  
+  task := Task{"task1", "the first example task"}
+  tasks := []Task{task}
+  tasks = append(tasks, Task{"task2", "the second example task"})
+  fmt.Println(tasks)
+  
+
+  epic := Epic{"epic1", tasks}
+  epics := []Epic{epic}
+  epics = append(epics, Epic{"epic2", tasks})
+  fmt.Println(epics)
+
+  project := Project{"project1", epics}
+  projects := []Project{project}
+  projects = append(projects ,Project{"project1", epics})
+  fmt.Println(projects[0].title)
+  fmt.Println(projects[0].epics[0].title)
+
+
+
 }
