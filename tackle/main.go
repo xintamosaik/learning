c487670762f53b074bdd0abe30eb819f40f86782
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-13 22:17:27 +0200

    read file line by line

--- tackle/main.go
+++ tackle/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+  "bufio"
+  "fmt"
+  "os"
+)
+
 
 func main() {
+  const FILENAME = "todo.md"
   fmt.Println("started..")
+  file, err := os.Open(FILENAME)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+      
+        fmt.Println(scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Println("Error:", err)
+    }
+
 }
 
