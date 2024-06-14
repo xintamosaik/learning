8a565a320b34ac51493dd36c1fd4d327f5c6c391
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-14 02:48:00 +0200

    make json work

--- tackle/main.go
+++ tackle/main.go
@@ -2,73 +2,97 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"unicode"
+
 )
 
 type Task struct {
-	title       string
-	description string
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
 
 type Epic struct {
-	title string
-	tasks []Task
+	Title string `json:"title"`
+	Tasks []Task `json:"tasks"`
 }
 
 type Project struct {
-	title string
-	epics []Epic
+	Title string `json:"title"`
+	Epics []Epic `json:"epics"`
 }
 
 func main() {
 	fmt.Println("started..")
-	const FILENAME = "todo.md"
+	
+  h1 := func(w http.ResponseWriter, _ *http.Request) {
 
-	file, err := os.Open(FILENAME)
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
+
+    const FILENAME = "todo.md"
+
+    file, err := os.Open(FILENAME)
+    if err != nil {
+      fmt.Println("Error:", err)
+      os.Exit(1)
+    }
+
+    scanner := bufio.NewScanner(file)
+
+    if err := scanner.Err(); err != nil {
+      log.Fatal(err)
+      os.Exit(1)
+    }
+
+    project := Project{FILENAME, []Epic{}}
+    currentEpic := 0
+    currentTask := 1
+    project.Epics = append(project.Epics, Epic{fmt.Sprintf("%v", currentEpic), []Task{}})
+
+    for scanner.Scan() {
+      line := scanner.Text()
+      trimmed := strings.TrimSpace(line)
+
+      if len(trimmed) > 0 {
+        if unicode.IsDigit(rune(line[0])) {
+          currentEpic++
+          currentTask = 1
+          project.Epics = append(project.Epics, Epic{fmt.Sprintf("%v", currentEpic), []Task{}})
+          project.Epics[currentEpic].Title = trimmed
+        }
+        if rune(line[0]) == 32 {
+
+          project.Epics[currentEpic].Tasks = append(project.Epics[currentEpic].Tasks, Task{fmt.Sprintf("%v", currentTask), trimmed})
+          currentTask++
+        }
+
+      }
+    }
+
+    jsonData, err := json.Marshal(project)
+    if err != nil {
+      fmt.Printf("could not marshal json: %s\n", err)
+      return
+    }
+    
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    //w.Header().Set("Content-Type", "application/json")
+  
+   
+		w.WriteHeader(http.StatusOK)
+    w.Write(jsonData)
+    // json.NewEncoder(w).Encode(project)
+	
+    
 	}
 
-	scanner := bufio.NewScanner(file)
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
+	http.HandleFunc("/", h1)
 
-	project := Project{FILENAME, []Epic{}}
-	currentEpic := 0
-	currentTask := 1
-	project.epics = append(project.epics, Epic{fmt.Sprintf("%v", currentEpic), []Task{}})
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		trimmed := strings.TrimSpace(line)
-
-		if len(trimmed) > 0 {
-			if unicode.IsDigit(rune(line[0])) {
-				currentEpic++
-				currentTask = 1
-				project.epics = append(project.epics, Epic{fmt.Sprintf("%v", currentEpic), []Task{}})
-				project.epics[currentEpic].title = trimmed
-			}
-			if rune(line[0]) == 32 {
-
-				project.epics[currentEpic].tasks = append(project.epics[currentEpic].tasks, Task{fmt.Sprintf("%v", currentTask), trimmed})
-				currentTask++
-				fmt.Println("[task] " + trimmed)
-			}
-
-		} else {
-			fmt.Println("[empty]")
-		}
-	}
-
-	fmt.Println(project)
-
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
