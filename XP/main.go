561d0d96e710e1d87737900fb1198a731e587de4
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-16 00:43:21 +0200

    more experimentation

--- XP/main.go
+++ XP/main.go
@@ -6,46 +6,41 @@ import (
     "net/http"
     "path/filepath"
 )
-func routeRequest(w http.ResponseWriter, r *http.Request) {
+func logHeader (r *http.Request) {
 	fmt.Println(r.Method);
-
 	for name, headers := range r.Header {
 		for _, h := range headers {
 			fmt.Printf("%v: %v\n", name, h)
 		}
 	}
+}
+func routeRequest(w http.ResponseWriter, r *http.Request) {
+	logHeader(r)
 
 	http.ServeFile(w, r, filepath.Join("", "index.html"))
 }
 
 func serveBacklog(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Method);
-
-	for name, headers := range r.Header {
-		for _, h := range headers {
-			fmt.Printf("%v: %v\n", name, h)
-		}
-	}
-
+	logHeader(r)
 	http.ServeFile(w, r, filepath.Join("", "backlog.html"))
+}
+func serveCss(w http.ResponseWriter, r *http.Request) {
+	logHeader(r)
+
+	http.ServeFile(w, r, filepath.Join("", "index.css"))
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Method);
-
-	for name, headers := range r.Header {
-		for _, h := range headers {
-			fmt.Printf("%v: %v\n", name, h)
-		}
-	}
+	logHeader(r)
 
 	http.ServeFile(w, r, filepath.Join("", "index.html"))
 }
 
 func main() {
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/home", serveHome)
-	http.HandleFunc("/backlog", serveBacklog)
+	http.HandleFunc("GET /index.css", serveCss)
+	http.HandleFunc("GET /", serveHome)
+	http.HandleFunc("GET /home", serveHome)
+	http.HandleFunc("GET /backlog", serveBacklog)
 
     // Start the server on port 8080
     log.Println("Starting server on :8080")
