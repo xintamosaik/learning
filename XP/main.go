0c52bc96a8160204d3f062706178e78421b54e71
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-15 23:51:29 +0200

    initial

---
+++ XP/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "path/filepath"
+)
+
+
+
+func routeRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method);
+
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			fmt.Printf("%v: %v\n", name, h)
+		}
+	}
+
+	http.ServeFile(w, r, filepath.Join("", "index.html"))
+}
+
+func main() {
+	http.HandleFunc("/", routeRequest)
+
+    // Start the server on port 8080
+    log.Println("Starting server on :8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatalf("Server failed: %v", err)
+    }
+}
