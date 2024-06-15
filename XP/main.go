449bb876904e2a05c1ea6d5df4370d32b735e76a
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-16 01:34:34 +0200

    working mvp

--- XP/main.go
+++ XP/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
+
 )
 
+
 func main() {
-	
+
 	mux := http.NewServeMux()
 
 	rootDir := http.Dir("./static")
@@ -15,7 +17,10 @@ func main() {
 	mux.Handle("/", rootServer)
 	mux.Handle("/home", rootServer)
 	mux.Handle("/backlog", rootServer)
-	
+
+	mux.Handle("/footer", rootServer)
+	mux.Handle("/header", rootServer)
+
 	err := http.ListenAndServe(":5555", mux)
 	if err != nil {
 		log.Fatal(err)
