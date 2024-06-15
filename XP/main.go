5666cf4d4cdffcf8796600aab35481cf685fa651
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-16 00:58:54 +0200

    experiment

--- XP/main.go
+++ XP/main.go
@@ -1,50 +1,28 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
-    "path/filepath"
 )
-func logHeader (r *http.Request) {
-	fmt.Println(r.Method);
-	for name, headers := range r.Header {
-		for _, h := range headers {
-			fmt.Printf("%v: %v\n", name, h)
-		}
-	}
-}
-func routeRequest(w http.ResponseWriter, r *http.Request) {
-	logHeader(r)
-
-	http.ServeFile(w, r, filepath.Join("", "index.html"))
-}
-
-func serveBacklog(w http.ResponseWriter, r *http.Request) {
-	logHeader(r)
-	http.ServeFile(w, r, filepath.Join("", "backlog.html"))
-}
-func serveCss(w http.ResponseWriter, r *http.Request) {
-	logHeader(r)
-
-	http.ServeFile(w, r, filepath.Join("", "index.css"))
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	logHeader(r)
-
-	http.ServeFile(w, r, filepath.Join("", "index.html"))
-}
 
 func main() {
-	http.HandleFunc("GET /index.css", serveCss)
-	http.HandleFunc("GET /", serveHome)
-	http.HandleFunc("GET /home", serveHome)
-	http.HandleFunc("GET /backlog", serveBacklog)
+	
+	mux := http.NewServeMux()
 
-    // Start the server on port 8080
-    log.Println("Starting server on :8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatalf("Server failed: %v", err)
-    }
+	rootDir := http.Dir("./static")
+	rootServer := http.FileServer(rootDir)
+	mux.Handle("/", rootServer)
+	
+	// homeDir := http.Dir("./static/home")
+	//homeServer := http.FileServer(homeDir)
+	mux.Handle("/home", rootServer)
+
+	// backDir := http.Dir("./static/back")
+	// backServer := http.FileServer(backDir)
+	mux.Handle("/backlog", rootServer)
+	
+	err := http.ListenAndServe(":5555", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
