2d1dc39cd66d48af319aaea57c4285bc631bece4
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-06-16 02:24:24 +0200

    experiment htmx spa

---
+++ XP3/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"path/filepath"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func backlog(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, filepath.Join("content", "backlog.html"))
+}
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "home")
+}
+func main() {
+
+	mux := http.NewServeMux()
+
+	// basically ./static/index.html
+	rootDir := http.Dir("./static")
+	rootServer := http.FileServer(rootDir)
+	mux.Handle("/", rootServer)
+
+	// these are callbacks for the navigation that should send back html for htmx
+	mux.HandleFunc("/home", home)
+	mux.HandleFunc("/backlog", backlog)
+
+	err := http.ListenAndServe(":5555", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
